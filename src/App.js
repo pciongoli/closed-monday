8ccff378850894e8c59eff8e539765d94cd0cc72
pcion <patrick.ciongoli@gmail.com>
2023-10-31 12:14:45 -0700

    updated router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./App.css";
 
 function App() {
    return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename="/">
          <NavBar />
          <div className="app-container">
             <div className="main-content">
