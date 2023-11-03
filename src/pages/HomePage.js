d6fd0fc232e374f5bb3ab8fd7fda08c53ee9d3c4
pcion <patrick.ciongoli@gmail.com>
2023-11-03 01:26:19 -0700

    updated homepage and homepage css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,28 +1,37 @@
 import React from "react";
-import heroImage from "../assets/image/hero-image.png";
+import heroVideo from "../assets/video/hero-video.mp4"; // Update the import path as necessary
 import videoEditorImage from "../assets/image/video-editor-img2.png";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "../styles/HomePage.css";
+
+const settings = {
+   dots: true,
+   infinite: true,
+   speed: 500,
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: true,
+};
 
 const HomePage = () => (
    <div className="home">
       <div className="hero">
-         <img src={heroImage} alt="Promotional banner" className="hero-image" />
-
+         {/* Replace img tag with video tag */}
+         <video className="hero-video" autoPlay loop muted>
+            <source src={heroVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+         </video>
          <div className="action-buttons">
             <button onClick={() => (window.location.href = "/contact")}>
                Get in Contact with Us
             </button>
             <button onClick={() => (window.location.href = "/portfolio")}>
-               See some of our work!{" "}
+               See some of our work!
             </button>
          </div>
       </div>
-
-      <h2 className="introduction">
-         Your destination for high-quality video editing. From short clips to
-         full-length productions, we bring your vision to life.
-      </h2>
       <div className="clients-section">
          <h2>Our Collaborators</h2>
          <div className="widgets-container">
@@ -42,7 +51,7 @@ const HomePage = () => (
             <div className="spotify-widget">
                <h3>UNDER THE INFLUENCE</h3>
                <iframe
-                  title="Spotify Widget - JOHNNY DRINKS"
+                  title="Spotify Widget - UNDER THE INFLUENCE"
                   style={{ borderRadius: "12px" }}
                   src="https://open.spotify.com/embed/show/5NDVTKGjiZhrth5hJbfvsA/video?utm_source=generator"
                   width="624"
@@ -55,7 +64,7 @@ const HomePage = () => (
             <div className="spotify-widget">
                <h3>SUBURB TALKS</h3>
                <iframe
-                  title="Spotify Widget - JOHNNY DRINKS"
+                  title="Spotify Widget - SUBURB TALKS"
                   style={{ borderRadius: "12px" }}
                   src="https://open.spotify.com/embed/show/4YLbUxILwRNN9e1UUCDV1D?utm_source=generator"
                   width="100%"
@@ -67,10 +76,8 @@ const HomePage = () => (
             </div>
          </div>
       </div>
-
       <div className="services-overview">
          <h2>Services We Offer</h2>
-
          <div className="services-container">
             <div className="services-list">
                <ul>
@@ -82,7 +89,6 @@ const HomePage = () => (
                   <li>TikTok video editing</li>
                </ul>
             </div>
-
             <button
                className="explore-services-btn"
                onClick={() => (window.location.href = "/services")}
@@ -91,35 +97,98 @@ const HomePage = () => (
             </button>
          </div>
       </div>
-
+      <div className="short-videos-section">
+         <h2>Short Videos & TikToks</h2>
+         <div className="video-container">
+            <iframe
+               width="315"
+               height="560"
+               src="https://www.youtube.com/embed/bTi_OD9R1ZE"
+               title="YouTube video player"
+               frameborder="0"
+               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+               allowfullscreen
+            ></iframe>
+            <iframe
+               width="315"
+               height="560"
+               src="https://www.youtube.com/embed/tN6iz9P6A3c"
+               title="YouTube video player"
+               frameborder="0"
+               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+               allowfullscreen
+            ></iframe>
+            <iframe
+               width="315"
+               height="560"
+               src="https://www.youtube.com/embed/okyYbCBIwFo"
+               title="YouTube video player"
+               frameborder="0"
+               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+               allowfullscreen
+            ></iframe>
+            <iframe
+               width="315"
+               height="560"
+               src="https://www.youtube.com/embed/fPMvyeEZ9Hc"
+               title="YouTube video player"
+               frameborder="0"
+               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+               allowfullscreen
+            ></iframe>
+         </div>
+      </div>
       <div className="contact-section">
+         <div className="contact-text">Text us anytime at 200-CLOSEDMO</div>
+         <div className="contact-info">
+            <p>Got a project in mind?</p>
+            <p>Let's talk and turn your vision into reality.</p>
+            <p>Reach out to us and get your quote today!</p>
+         </div>
          <img
             src={videoEditorImage}
             alt="Video Editor"
             className="editor-image"
          />
-         <div className="contact-text">Text us anytime at 200-CLOSEDMO</div>
       </div>
-
       <div className="testimonials">
          <h2>What Our Clients Say</h2>
-
-         <div className="testimonial-container">
-            <div className="avatar"></div>
-            <p>
-               "Closed On Mondays provided exceptional editing services for our
-               corporate videos. Highly recommended!"
-               <br />- John Doe, CEO of ABC Corp
-            </p>
-         </div>
-         <div className="testimonial-container">
-            <div className="avatar"></div>
-            <p>
-               "The attention to detail and creativity brought our documentary
-               to life. Can't wait for our next project!"
-               <br />- Jane Smith, Documentary Producer
-            </p>
-         </div>
+         <Slider {...settings}>
+            <div className="testimonial-container">
+               <div className="avatar"></div>
+               <div class="testimonial-text">
+                  <p>
+                     "Closed On Mondays provided exceptional editing services
+                     for our corporate videos. Highly recommended!"
+                  </p>
+               </div>
+               <div class="testimonial-author">- John Doe, CEO of ABC Corp</div>
+            </div>
+            <div className="testimonial-container">
+               <div className="avatar"></div>
+               <div class="testimonial-text">
+                  <p>
+                     "The attention to detail and creativity brought our
+                     documentary to life. Can't wait for our next project!"
+                  </p>
+               </div>
+               <div class="testimonial-author">
+                  - Jane Smith, Documentary Producer
+               </div>
+            </div>
+            <div className="testimonial-container">
+               <div className="avatar"></div>
+               <div class="testimonial-text">
+                  <p>
+                     "Their quick turnaround and innovative approach made a huge
+                     impact on our marketing campaign."
+                  </p>
+               </div>
+               <div class="testimonial-author">
+                  - Michael Lee, Marketing Director
+               </div>
+            </div>
+         </Slider>
       </div>
    </div>
 );
